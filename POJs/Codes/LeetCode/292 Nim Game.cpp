342ad442582da61254eac98b0d76ccec35a2dfc1
QuincyHu <hu.quincy@outlook.com>
2016-07-27 11:43:14 +0800

    Nim Game

--- POJs/Codes/LeetCode/292 Nim Game.cpp
+++ POJs/Codes/LeetCode/292 Nim Game.cpp
@@ -6,6 +6,15 @@ using namespace std;
 // https://leetcode.com/problems/nim-game/
 
 bool canWinNim(int n)
+{	
+	// 只要是4的倍数都会输，否则会赢，因而变成了判断n是否是4的倍数。
+	// 可以判断n的最后2bit是否为0来判断
+	return (n & 0x3);
+}
+
+
+TEST(LeetCode, tCanWinNim)
 {
-	return false;
+	ASSERT_TRUE(canWinNim(5));
+	ASSERT_FALSE(canWinNim(228));
 }
