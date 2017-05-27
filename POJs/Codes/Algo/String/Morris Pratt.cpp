68d0b91802f5185c38996e155aa98ae60a9a3cfd
QuincyHu <hu.quincy@outlook.com>
2017-05-27 17:46:16 +0800

    Add Morris Pratt.cpp

---
+++ POJs/Codes/Algo/String/Morris Pratt.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+
+using namespace std;
+
+void preMp(char* x, int m, int mpNext[])
+{
+	mpNext[0] = -1;
+	for(int i=0; i<m; ++i)
+	{
+		int j = -1;
+		while (j > -1 && x[i] != x[j])
+		{
+			j = mpNext[j];
+		}
+
+		mpNext[++i] = ++j;
+	}
+}
+
+int MorrisPratt(char* S,int N,char* P,int M)
+{
+	int result = -1;
+	int mpNext[100] = {0};
+	// Ô¤´¦Àí
+	preMp(P,M,mpNext);
+
+	// ËÑË÷
+	for (int i=0; i<N; ++i)
+	{
+		int j=0;
+		while (j > -1 && S[i] != P[j])
+		{
+			j = mpNext[j];
+		}
+		
+		j++;
+		i++;
+		if (j >= M)
+		{
+			result = j - i;
+			break;
+		}
+	}
+}
